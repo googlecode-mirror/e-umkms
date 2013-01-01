cfc43ead0262a1b4d3c60a151462b6ddee56d903
primafandi@gmail.com <primafandi@gmail.com@d8341b1e-d457-3d69-60dd-e5950d99f4c8>
2013-01-01 16:52:40 +0000

    profil dan profil2 merupakan php code tentang fitur "Profil UMKMS" pada web e-umkms

---
+++ trunk/php code umkms baru/profil.php
@@ -0,0 +1,90 @@
+<html>
+<head> <link href="bootstrap.css" rel="stylesheet" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+<title>UMKM-SBY</title>
+<link rel="shortcut icon" href="favicon1.ico">
+
+</head>
+
+<link rel="stylesheet" type="css/text" href="../bootstrap.css" />
+<h1>
+<center><img src="header.jpg" /></center>
+
+<form id="form1" name="form1" method="post" action="login.php" align=right>
+          <tr>
+            <td><input name="user" type="text" id="user" value="username" /></td>
+          </tr>
+          <tr>
+            <td><input name="pass" type="password" id="pass" value="password" /></td>
+          </tr>
+		  
+</tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td><input type="submit" name="Submit" value="Login" /></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+        </table>
+              </form>
+</h1>
+
+<ul class="nav nav-pills">
+  <li class="active"><a href="home.php">Home</a></li>
+  <li class="active"><a href="about.php">About Us</a></li>
+  <li class="active"><a href="#">Artikel</a></li>
+  <li class="active"><a href="profil.php">Profil UMKMS</a></li>
+  <li class="active"><a href="#">Produk Jual</a></li>
+  <li class="active"><a href="kontak.php">Kontak Kami</a></li>
+  <li class="active"><a href="register.php">Sign Up</a></li>
+  <li class="active"><a href="#">Lupa Password</a></li>
+</ul>
+</br>
+</br>
+<center>
+<strong><font size="20">Profil UMKM</font></strong>
+</center>
+</br>
+</br>
+
+
+<table border=1 bordercolor=black align="center">
+<tr>
+<td><a href="abp.php"><img src="abp.jpg" /></a></td> 
+<td><a href="dc.php"><img src="dc.jpg" /></td>
+<tr>
+<tr>
+<td><a href="csp.php"><img src="csp.jpg" /></a></td> 
+<td><a href="dlm.php"><img src="dlm.jpg" /></td>
+</tr>
+<tr>
+<td><a href="cs.php"><img src="cs.jpg" /></td>
+<td><a href="vp.php"><img src="vp.jpg" /></td>
+</tr>
+<tr>
+<td><a href="ri.php"><img src="ri.jpg" /></td> 
+<td><a href="wd.php"><img src="wd.jpg" /></td>
+</tr>
+</table>
+</br>
+<center><table width=500 border=1 bordercolor=yellow><tr><td>
+<center>
+Untuk melihat profil dan informasi dari UMKM yang ada di Surabaya, anda bisa meng-klik 
+salah satu logo UMKM tersebut yang ada pada tabel di atas
+</center>
+</td></tr></table></center>
+
+<body style="background-color:yellow;">
+<form method="POST" action="register.php">  
+
+<footer><p style="font-family:Arial;"><center><strong><marquee behavior=alternate>
+
+	Website ini bukan merupakan website resmi UMKM Surabaya, hanya merupakan web tugas kuliah
+</marquee></strong></center></p></footer>
+
+
+</body>  
+</html>
---
+++ trunk/php code umkms baru/profil2.php
@@ -0,0 +1,76 @@
+<?php
+	session_start();
+	if (!isset ($_SESSION['register'])){
+		header("location: home.php");
+		exit();
+	}
+?>
+
+<html>
+<head> <link href="bootstrap.css" rel="stylesheet" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+<title>UMKM-SBY</title>
+<link rel="shortcut icon" href="favicon1.ico">
+
+</head>
+
+<link rel="stylesheet" type="css/text" href="../bootstrap.css" />
+<h1>
+<center><img src="header.jpg" /></center>
+</h1>
+
+<ul class="nav nav-pills">
+  <li class="active"><a href="home2.php">Home</a></li>
+  <li class="active"><a href="about2.php">About Us</a></li>
+  <li class="active"><a href="#">Artikel</a></li>
+  <li class="active"><a href="profil2.php">Profil UMKMS</a></li>
+  <li class="active"><a href="#">Produk Jual</a></li>
+  <li class="active"><a href="kontak2.php">Kontak Kami</a></li>
+  <li class="active"><a href="home.php">Logout</a></li>
+</ul>
+</br>
+</br>
+<center>
+<strong><font size="20">Profil UMKM</font></strong>
+</center>
+</br>
+</br>
+<table border=1 bordercolor=black align="center">
+<tr>
+<td><a href="abp2.php"><img src="abp.jpg" /></a></td> 
+<td><a href="dc2.php"><img src="dc.jpg" /></td>
+<tr>
+<tr>
+<td><a href="csp2.php"><img src="csp.jpg" /></a></td> 
+<td><a href="dlm2.php"><img src="dlm.jpg" /></td>
+</tr>
+<tr>
+<td><a href="cs2.php"><img src="cs.jpg" /></td>
+<td><a href="vp2.php"><img src="vp.jpg" /></td>
+</tr>
+<tr>
+<td><a href="ri2.php"><img src="ri.jpg" /></td> 
+<td><a href="wd2.php"><img src="wd.jpg" /></td>
+</tr>
+</table>
+</br>
+<center><table width=500 border=1 bordercolor=yellow><tr><td>
+<center>
+Untuk melihat profil dan informasi dari UMKM yang ada di Surabaya, anda bisa meng-klik 
+salah satu logo UMKM tersebut yang ada pada tabel di atas
+</center>
+</td></tr></table></center>
+
+<body style="background-color:yellow;">
+<form method="POST" action="register.php">  
+
+<footer><p style="font-family:Arial;"><center><strong><marquee behavior=alternate>
+
+	Website ini bukan merupakan website resmi UMKM Surabaya, hanya merupakan web tugas kuliah
+</marquee></strong></center></p></footer>
+
+
+</body>  
+</html>
+
