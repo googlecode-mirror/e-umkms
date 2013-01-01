30b780ed1590f059f15b74489e68048b09203859
primafandi@gmail.com <primafandi@gmail.com@d8341b1e-d457-3d69-60dd-e5950d99f4c8>
2013-01-01 14:38:43 +0000

    kontak dan kontak2 merupakan php code tentang fitur "Contact" pada web e-umkms

---
+++ trunk/php code umkms baru/kontak.php
@@ -0,0 +1,82 @@
+<html>
+<head> <link href="bootstrap.css" rel="stylesheet" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+<title>UMKM-SBY</title>
+<link rel="shortcut icon" href="favicon1.ico">
+
+</head>
+
+<link rel="stylesheet" type="css/text" href="../bootstrap.css" />
+<h1>
+<center><img src="header.jpg" /></center>
+
+<form id="form1" name="form1" method="post" action="login.php" align=right>
+          <tr>
+            <td><input name="user" type="text" id="user" value="username" /></td>
+          </tr>
+          <tr>
+            <td><input name="pass" type="password" id="pass" value="password" /></td>
+          </tr>
+		  
+</tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td><input type="submit" name="Submit" value="Login" /></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+        </table>
+              </form>
+</h1>
+
+<ul class="nav nav-pills">
+  <li class="active"><a href="home.php">Home</a></li>
+  <li class="active"><a href="about.php">About Us</a></li>
+  <li class="active"><a href="#">Artikel</a></li>
+  <li class="active"><a href="profil.php">Profil UMKMS</a></li>
+  <li class="active"><a href="#">Produk Jual</a></li>
+  <li class="active"><a href="kontak.php">Kontak Kami</a></li>
+  <li class="active"><a href="register.php">Sign Up</a></li>
+  <li class="active"><a href="#">Lupa Password</a></li>
+</ul>
+</br>
+</br>
+<center>
+<strong><font size="20">Kontak Kami…</font></strong>
+</center>
+</br>
+</br>
+
+<center>
+<table width=500 border=3 bordercolor=yellow style="border-collapse:collapse"><tr><td>
+<center>Untuk mempermudah anda dalam menghubungi kami, kami memberikan informasi mengenai kontak dari masing masing tim developer 
+sehingga dapat melakukan komunikasi dengan kami secara mudah, berikut :
+</br>
+</br>
+<table width=300 border=3 bordercolor=magenta style="border-collapse:collapse"><tr><td>
+<center>
+Prima	: 083857008000 </br>
+Danis	: 085733577739 </br>
+Putri : 087853358866 </br>
+</center>
+</td></tr></table>
+ 
+</center>
+</td></tr></table>
+</center>
+</br>
+
+<body style="background-color:yellow;">
+<form method="POST" action="register.php">  
+
+<footer><p style="font-family:Arial;"><center><strong><marquee behavior=alternate>
+
+	Website ini bukan merupakan website resmi UMKM Surabaya, hanya merupakan web tugas kuliah
+</marquee></strong></center></p></footer>
+
+
+</body>  
+</html>
