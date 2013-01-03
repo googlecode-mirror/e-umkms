189a1076b3a78c2f8004d14dbf3b2e16b06cb97c
primafandi <primafandi@gmail.com>
2013-01-03 16:55:54 +0000

    php code tentang fitur "Artikel"

---
+++ php code umkms baru/artikel.php
@@ -0,0 +1,89 @@
+<html>
+<head> <link href="bootstrap.css" rel="stylesheet" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+<title>UMKM-SBY</title>
+<link rel="shortcut icon" href="favicon1.ico">
+
+</head>
+
+<link rel="stylesheet" type="css/text" href="../bootstrap.css" />
+<h1>
+<center><img src="header.jpg" /></center>
+
+<form id="form1" name="form1" method="post" action="login.php" align=right>
+          <tr>
+            <td><input name="user" type="text" id="user" value="username" /></td>
+          </tr>
+          <tr>
+            <td><input name="pass" type="password" id="pass" value="password" /></td>
+          </tr>
+		  
+</tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td><input type="submit" name="Submit" value="Login" /></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+        </table>
+              </form>
+</h1>
+
+<ul class="nav nav-pills">
+  <li class="active"><a href="home.php">Home</a></li>
+  <li class="active"><a href="about.php">About Us</a></li>
+  <li class="active"><a href="artikel.php">Artikel</a></li>
+  <li class="active"><a href="profil.php">Profil UMKMS</a></li>
+  <li class="active"><a href="#">Produk Jual</a></li>
+  <li class="active"><a href="kontak.php">Kontak Kami</a></li>
+  <li class="active"><a href="register.php">Sign Up</a></li>
+  <li class="active"><a href="#">Lupa Password</a></li>
+</ul>
+</br>
+</br>
+<center>
+<strong><font size="20">Artikel</font></strong>
+</center>
+</br>
+</br>
+
+<center>
+<table width=500 border=3 bordercolor=yellow style="border-collapse:collapse"><tr><td>
+<center>
+<font size="14">Kredit Usaha Rakyat (KUR)</font>
+</center>
+</br>
+<center>Pada tanggal 5 November 2007, Presiden meluncurkan Kredit Usaha Rakyat (KUR), dengan fasilitas penjaminan kredit dari 
+pemerintah melalui PT Aksrindo dan Perum Jamkrindo. Adapun Bank Pelaksana yang menyalurkan KUR ini adalah Bank BRI, 
+Bank Mandiri, Bang BNI, Banj BTN, Bank Syariah Mandiri, dan Bank Bukopin.
+KUR ini merupakan fasilitas pembiayaan yang dapat diakses oleh UMKM dan Koperasi terutama yang memiliki usaha yang layak 
+namun belum bankable. Maksudnya adalah usaha tersebut memiliki prospek bisnis yang baik dan memiliki kemampuan untuk mengembalikan.
+ UMKM dan Koperasi yang diharapkan dapat mengakses KUR adalah yang bergerak di sektor usaha produktif antara lain : pertanian, perikanan,
+ dan kelautan, perindustrian, kehutanan, dan jasa keuangan simpan pinjam. Penyaluran KUR dapat dilakukan langsung, maksudnya UMKM 
+ dan Koperasi dapat langsung mengakses KUR di kantor Cabang atau Kantor Cabang Pembantu Bank Pelaksana. Untuk lebih mendekatkan pelayanan 
+ kepada usaha mikro, maka penyaluran KUR dapat juga dilakukan secara tidak langsung, maksudnya usaha mikro dapat mengakses KUR melalui Lembaga
+ Keuangan Mikro dan KSPIUSP Koperasi, atau melalui kegiatan linkage program lainnya yang bekerjasama dengan Bank pelaksana.
+Memperhatikan kemanfaatan KUR, kedepan penyalurannya semakin diperluas dengan prosedur diusahakan semakin mudah dan proses yang lebih cepat. 
+Disamping itu mulai tahun 2010 Bank Pelaksana KUR menjadi 19 Bank yaitu ; PT. BRI, PT. Mandiri, PT. BNI, PT. BTN, PT. Bank Syariah Mandiri
+ dan PT. Bukopin ditambah 13 Bank Pembangunan Daerah (BPD) yaitu : PT. Bank DKI, PT. BPD Jabar dan Banten, PT. BPD Jawa Tengah, 
+ PT. BPD Jawa Timur, BPD DI Yogyakarta, PT. BPD Nusa Tenggara Barat, PT. BPD Sumatra Barat, PT. BPD Kalimantan Barat, PT. Bank Pembangunan Kalteng,
+ BPD Kalimantan Selatan, PT. BPD Sulawesi Utara, PT. BPD Maluku dan PT. BPD Papua.
+ </center>
+</td></tr></table>
+</center>
+</br>
+
+<body style="background-color:yellow;">
+<form method="POST" action="register.php">  
+
+<footer><p style="font-family:Arial;"><center><strong><marquee behavior=alternate>
+
+	Website ini bukan merupakan website resmi UMKM Surabaya, hanya merupakan web tugas kuliah
+</marquee></strong></center></p></footer>
+
+
+</body>  
+</html>
