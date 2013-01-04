04d050e3a59c1ed03603359f394c7a8f2045aff9
primafandi@gmail.com <primafandi@gmail.com@d8341b1e-d457-3d69-60dd-e5950d99f4c8>
2013-01-04 14:31:35 +0000

    php code yang berisi macam macam fungsi

--- trunk/php code umkms baru/function.php
+++ trunk/php code umkms baru/function.php
@@ -1,14 +1,13 @@
 <?php
 include "connect.php";
 class f{
-	function register(){
+	function tabeluser(){
 	if(isset($_POST['Save'])){
-	if($_POST['user']!=''&& $_POST['firstname']!=''&& $_POST['lastname']!=''&& $_POST['firstname']!='' ){
-		$user=$_POST['user'];
-		$firstname=$_POST['firstname'];
-		$lastname=$_POST['lastname'];
-		$pass=$_POST['pass'];
-		$sql="insert into register values('".$user."','".$firstname."','".$lastname."','".$pass."')";
+	if($_POST['userid']!=''&& $_POST['password']!=''&& $_POST['level']!='' ){
+		$userid=$_POST['userid'];
+		$password=$_POST['password'];
+		$level=$_POST['level'];
+		$sql="insert into tabeluser values('".$userid."','".$password."','".$level."')";
 		mysql_query($sql) or (mysql_error());
 	}}
 	}
