7a5b4dd618056ea35cd435d3654261401d74ccb8
primafandi@gmail.com <primafandi@gmail.com@d8341b1e-d457-3d69-60dd-e5950d99f4c8>
2013-01-04 14:32:15 +0000

    merupakan php code untuk fitur artikel

---
+++ trunk/php code umkms baru/psu.php
@@ -0,0 +1,114 @@
+<html>
+<head> <link href="bootstrap.css" rel="stylesheet" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+<title>UMKM-SBY</title>
+<link rel="shortcut icon" href="favicon1.ico">
+
+</head>
+
+<link rel="stylesheet" type="css/text" href="../bootstrap.css" />
+<h1>
+<marquee direction=down loop=true height="50"><center><img src="header.jpg" /></center></marquee>
+<script type="text/javascript">
+function UR_Start() 
+{
+	UR_Nu = new Date;
+	UR_Indhold = showFilled(UR_Nu.getHours()) + ":" + showFilled(UR_Nu.getMinutes()) + ":" + showFilled(UR_Nu.getSeconds());
+	document.getElementById("ur").innerHTML = UR_Indhold;
+	setTimeout("UR_Start()",1000);
+}
+function showFilled(Value) 
+{
+	return (Value > 9) ? "" + Value : "0" + Value;
+}
+
+</script>
+<font id="ur" size="6" face="Trebuchet MS, Verdana, Arial, sans-serif" color=pink></font>
+
+<form id="form1" name="form1" method="post" action="login.php" align=right>
+          <tr>
+            <td><input name="user" type="text" id="user" value="username" /></td>
+          </tr>
+          <tr>
+            <td><input name="pass" type="password" id="pass" value="password" /></td>
+          </tr>
+		  
+</tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td><input type="submit" name="Submit" value="Login" /></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+          </tr>
+        </table>
+              </form>
+</h1>
+
+<ul class="nav nav-pills">
+  <li class="active"><a href="home.php">Home</a></li>
+  <li class="active"><a href="about.php">About Us</a></li>
+  <li class="active"><a href="artikel.php">Artikel</a></li>
+  <li class="active"><a href="profil.php">Profil UMKMS</a></li>
+  <li class="active"><a href="kontak.php">Kontak Kami</a></li>
+  <li class="active"><a href="register.php">Sign Up</a></li>
+  <li class="active"><a href="#">Lupa Password</a></li>
+</ul>
+</br>
+</br>
+<center>
+<strong><font size="20">Artikel</font></strong>
+</center>
+</br>
+</br>
+
+<center>
+<table width=500 border=3 bordercolor=yellow style="border-collapse:collapse"><tr><td>
+<center>
+<font size="6">Kredit Usaha Rakyat (KUR)</font>
+</center>
+</br>
+<center>
+Pengembangan terhadap sektor swasta merupakan suatu hal yang tidak diragukan lagi perlu untuk dilakukan.
+UKM memiliki peran penting dalam pengembangan usaha di Indonesia.UKM juga merupakan cikal bakal dari tumbuhnya usaha besar.
+”Hampir semua usaha besar berawal dari UKM.Usaha kecil menengah (UKM) harus terus ditingkatkan (up grade) dan aktif agar dapat maju dan 
+bersaing dengan perusahaan besar. Jika tidak, UKM di Indonesia yang merupakan jantung perekonomian Indonesia tidak akan bisa maju dan berkembang.
+Satu hal yang perlu diingat dalam pengembangan UKM adalah bahwa langkah ini tidak semata-mata merupakan langkah yang harus diambil oleh 
+Pemerintah dan hanya menjadi tanggung jawab Pemerintah.Pihak UKM sendiri sebagai pihak yang dikembangkan, dapat mengayunkan langkah 
+bersama-sama dengan Pemerintah.Selain Pemerintah dan UKM, peran dari sektor Perbankan juga sangat penting terkait dengan segala hal 
+mengenai pendanaan, terutama dari sisi pemberian pinjaman atau penetapan kebijakan perbankan. Lebih jauh lagi, terkait dengan ketersediaan 
+dana atau modal, peran dari para investor baik itu dari dalam maupun luar negeri, tidak dapat pula kita kesampingkan.
+Pemerintah pada intinya memiliki kewajiban untuk turut memecahkan tiga hal masalah klasik yang kerap kali menerpa UKM, yakni akses pasar,
+ modal, dan teknologi yang selama ini kerap menjadi pembicaraan di seminar atau konferensi. Secara keseluruhan, terdapat beberapa hal yang 
+ harus diperhatikan dalam melakukan pengembangan terhadap unit usaha UKM, antara lain kondisi kerja, promosi usaha baru, akses informasi, 
+ akses pembiayaan, akses pasar, peningkatan kualitas produk dan SDM, ketersediaan layanan pengembangan usaha, pengembangan cluster, jaringan 
+ bisnis, dan kompetisi.
+Perlu disadari, UKM berada dalam suatu lingkungan yang kompleks dan dinamis.Jadi, upaya mengembangkan UKM tidak banyak berarti bila tidak
+mempertimbangkan pembangunan (khususnya ekonomi) lebih luas. Konsep pembangunan yang dilaksanakan akan membentuk ‘aturan main’ bagi pelaku usaha 
+(termasuk UKM) sehingga upaya pengembangan UKM tidak hanya bisa dilaksanakan secara parsial, melainkan harus terintegrasi dengan pembangunan
+ ekonomi nasional dan dilaksanakan secara berkesinambungan. Kebijakan ekonomi (terutama pengembangan dunia usaha) yang ditempuh selama ini belum
+ menjadikan ikatan kuat bagi, terciptanya keterkaitan antara usaha besar ukm saat ini,Kementerian Koperasi dan Usaha Kecil Menengah berencana untuk
+ menciptakan 20 juta usaha kecil menengah baru tahun 2020.Tahun 2020 adalah masa yang menjanjikan begitu banyak peluang karena di tahun tersebut
+ akan terwujud apa yang dimimpikan para pemimpin ASEAN yang tertuang dalam Bali Concord II. Suatu komunitas ekonomi ASEAN, yang peredaran produk-produk
+ barang dan jasanya tidak lagi dibatasi batas negara, akan terwujud. Kondisi ini membawa sisi positif sekaligus negatif bagi UKM. Menjadi positif
+ apabila produk dan jasa UKM mampu bersaing dengan produk dan jasa dari negara-negara ASEAN lainnya, namun akan menjadi negatif apabila sebaliknya.
+ Untuk itu, kiranya penting bila pemerintah mendesain program yang jelas dan tepat sasaran serta mencanangkan penciptaan 20 juta UKM sebagai program nasional.
+</center>
+</td></tr></table>
+</center>
+</br>
+<center><input type="button" value="Kembali ke Menu Artikel" onclick="window.location='artikel.php'" /></center>
+</br>
+<body onload="UR_Start()"  style="background-color:yellow;">
+<form method="POST" action="register.php">  
+
+<footer><p style="font-family:Arial;"><center><strong><marquee behavior=alternate>
+
+	Website ini bukan merupakan website resmi UMKM Surabaya, hanya merupakan web tugas kuliah
+</marquee></strong></center></p></footer>
+
+
+</body>  
+</html>
