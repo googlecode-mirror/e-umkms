6506d57d75f57b59daf49a5ddaf1acb79a7e83a3
primafandi <primafandi@gmail.com>
2013-01-04 14:30:35 +0000

    about.php dan about2.php merupakan php code untuk fitur About Us artikel.php dan artikel2.php merupakan php code untuk fitur artikel home.php(guest) dan home2.php(registered user) home3.php(admin) merupakan php code untuk fitur Homepage kontak.php dan kontak2.php merupakan php code untuk fitur Contact Us profil.php dan profil2.php merupakan php code untuk fitur Profil umkms register.php merupakan php code untuk fitur sign up

--- php code umkms baru/register.php
+++ php code umkms baru/register.php
@@ -6,36 +6,52 @@
 <title>Sign Up</title>
 <link rel="shortcut icon" href="favicon1.ico">
 </head>
-<center><img src="header.jpg" /></center>
+<marquee direction=down loop=true height="50"><center><img src="header.jpg" /></center></marquee>
+<script type="text/javascript">
+function UR_Start() 
+{
+	UR_Nu = new Date;
+	UR_Indhold = showFilled(UR_Nu.getHours()) + ":" + showFilled(UR_Nu.getMinutes()) + ":" + showFilled(UR_Nu.getSeconds());
+	document.getElementById("ur").innerHTML = UR_Indhold;
+	setTimeout("UR_Start()",1000);
+}
+function showFilled(Value) 
+{
+	return (Value > 9) ? "" + Value : "0" + Value;
+}
+
+</script>
+<font id="ur" size="6" face="Trebuchet MS, Verdana, Arial, sans-serif" color=pink></font>
 
 <body>
-<body style="background-color:yellow;">
+<body onload="UR_Start()"  style="background-color:yellow;">
 <form id="form1" name="form1" method="post" action="">
-
+</br>
  <?php
 	include'connect.php';
 	include'function.php';
 	$f = new f;
-	$f->register();
+	$f->tabeluser();
 	if($f){
 		echo"<center><a  style=font-family:Arial class=btn btn-primary btn-small href=home.php><b>Login Now</b></a></center>";
 	}
 	
  ?>
  </br>
+ <center><input type="button" value="Kembali ke Menu Utama" onclick="window.location='home.php'" /></center>
+ </br>
  </strong></td>
   <div align="center" class="style2">
       <tr>
-        <td><input name="user" type="text" id="user" value="user" size="50" /></td>
+        <td><input name="userid" type="text" id="userid" value="userid" size="50" /></td>
       </tr>
-      <tr>
-        <td><input name="firstname" type="text" id="firstname" value="firstname" size="50" /></td>
-      </tr>
-      <tr>
-        <td><input name="lastname" type="text" id="lastname" value="lastname"  size="50" /></td>
-      </tr>
+	  </br>
 	  <tr>
-        <td><input name="pass" type="text" id="pass" value="pass" size="50" /></td>
+        <td><input name="password" type="password" id="password" value="password" size="50" /></td>
+      </tr>
+	  </br>
+	  <tr>
+        <td><input name="level" type="text" id="level" value="level" size="50" /></td>
       </tr>
       <tr>
         <td><span class="style2"></span></td>
